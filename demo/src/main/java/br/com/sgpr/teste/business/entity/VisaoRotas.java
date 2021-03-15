a43f0c480dfcc8d018195cc1122eb147d61eb36a
Abraao Lincol <abraao.lincol@gmail.com>
2021-03-15 12:19:58 -0300

    fix bagunça dos merge

--- demo/src/main/java/br/com/sgpr/teste/business/entity/VisaoRotas.java
+++ demo/src/main/java/br/com/sgpr/teste/business/entity/VisaoRotas.java
@@ -1,4 +1,4 @@
-package br.com.sgpr.teste.business;
+package br.com.sgpr.teste.business.entity;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -10,6 +10,8 @@ import javax.persistence.Table;
 public class VisaoRotas {
     private String idRota;
     private String nome;
+    private String origem;
+    private String destino;
 
     @Id
     @Column(name = "id_rota")
@@ -30,5 +32,21 @@ public class VisaoRotas {
     @Column(name = "nome_rota")
     public void setNome(String novoNome){
         nome = novoNome;
+    }
+
+    public String getOrigem() {
+        return origem;
+    }
+
+    public void setOrigem(String origem) {
+        this.origem = origem;
+    }
+
+    public String getDestino() {
+        return destino;
+    }
+
+    public void setDestino(String destino) {
+        this.destino = destino;
     }
 }
